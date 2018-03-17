f220b20b6cc969526b4e429d8cac516b87ee712a
Adam Barrell <a.j.barrell@gmail.com>
2018-03-17 17:16:15 +0000

    Trigger travis.

--- test/test.js
+++ test/test.js
@@ -2,6 +2,7 @@ describe('Booleans', () => {
     describe('Truth tests', () => {
         it('equals should return true', () => {
             expect(true).to.equal(true);
+            expect(false).to.equal(false);
         });
     });
 });
